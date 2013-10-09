a327c683dee443db249eadc7e6eaa84ab4a0b2f5
Lewis <lewis@ubuntu.(none)>
2013-10-09 15:30:36 -0700

    added routes. changed styles to black and colored. got colors matching.

---
+++ converter.js
@@ -0,0 +1,12 @@
+var tj = require('togeojson'),
+    fs = require('fs'),
+    // node doesn't have xml parsing or a dom. use jsdom
+    jsdom = require('jsdom').jsdom;
+
+var kml = jsdom(fs.readFileSync('data/bart.kml', 'utf8'));
+
+var converted = tj.kml(kml);
+
+fs.writeFileSync('bartRoutes.json', JSON.stringify(converted));
+
+// var converted_with_styles = tj.kml(kml, { styles: true });
